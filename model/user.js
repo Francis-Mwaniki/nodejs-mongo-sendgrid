a99f3bf832217146015d0d07d7fab9a5220d2e30
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-11-07 15:04:51 +0300

    better server

--- model/user.js
+++ model/user.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {
+  phoneNumber: {
     type: String,
-   
-    min: 6,
-    max: 255,
-  },
-  lastName: {
-    type: String,
-   
+    required: true,
     min: 6,
     max: 255,
   },
--- routes/auth.js
+++ routes/auth.js
@@ -23,8 +23,7 @@ router.post("/register", async (req, res) => {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
       const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
         email: req.body.email,
         password: hashedPassword,
         confirmPassword: hashedPassword,
@@ -73,7 +72,7 @@ router.post("/forgot-password", async (req, res) => {
     user.resetPasswordExpires = Date.now() + 3600000; // Token expires in 1 hour
     await user.save();
 
-    const resetLink = `$https://render.com/reset-password/${resetToken}`;
+    const resetLink = `https://nexti-authentication.vercel.app/UpdatePassword/${resetToken}`;
     const msg = {
       to: email,
       from: "francismwanik254@gmail.com",
